08c1868f9d420fb312a37a232470ab27a0f7511f
Ruben Beeftink <r.beeftink@enie.nl>
2024-09-23 14:37:53 +0200

    feature: setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginPage} from "./pages/authentication/login/login.page";
+import {RegisterPage} from "./pages/authentication/register/register.page";
+import {ShortUrlIndexPage} from "./pages/short-url/index/short-url-index.page";
+import {ShortUrlDetailPage} from "./pages/short-url/detail/short-url-detail.page";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShortUrlIndexPage,
+  },
+  {
+    path: 'short-url/detail',
+    component: ShortUrlDetailPage,
+  },
+  {
+    path: 'short-url/detail/:id',
+    component: ShortUrlDetailPage,
+  },
+  {
+    path: 'login',
+    component: LoginPage
+  },
+  {
+    path: 'register',
+    component: RegisterPage,
+  },
+];
